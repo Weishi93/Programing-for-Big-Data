145e285ec4cb1898b0356a3d396c5234a91342a6
Wei Shi <ws1196@nyu.edu>
2016-12-21 13:49:17 -0500

    Update MaxTemperatureReducer.java

--- hw1/MaxTemperatureReducer.java
+++ hw1/MaxTemperatureReducer.java
@@ -6,8 +6,8 @@ public class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntW
 	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 		int maxValue = Integer.MIN_VALUE; 
 		for (IntWritable value : values) {
-	      	maxValue = Math.max(maxValue, value.get());
-	    }
+	      		maxValue = Math.max(maxValue, value.get());
+	    	}
 		context.write(key, new IntWritable(maxValue)); 
 	}
 }
